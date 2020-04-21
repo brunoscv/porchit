fb65ea522da520eb9bd4fd4fd209d4411f450c59
Bruno Carvalho <brunocarvalho038@gmail.com>
2020-04-21 13:17:21 -0300

    texto modal

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -93,7 +93,7 @@
         <div class="modal-dialog" role="document">
             <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title" id="exampleModalLabel"><?= gettext('Members Per Family') ?></h5>
+                <h5 class="modal-title" id="exampleModalLabel"><?= gettext('ZipCodes Per Product') ?></h5>
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                 <span aria-hidden="true">&times;</span>
                 </button>
