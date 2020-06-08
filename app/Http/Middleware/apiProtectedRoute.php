23b802a73445183a11b008482b978f8fe5d45d97
Bruno Carvalho <brunocarvalho038@gmail.com>
2020-06-08 17:13:24 -0300

    alterando novamente middleware

--- app/Http/Middleware/apiProtectedRoute.php
+++ app/Http/Middleware/apiProtectedRoute.php
@@ -16,52 +16,12 @@ class apiProtectedRoute extends BaseMiddleware
      * @param  \Closure  $next
      * @return mixed
      */
-    public function handle($request, Closure $next)
-    {
-        // caching the next action
-        $response = $next($request);
+    // public function handle($request, Closure $next)
+    // {
+    //     // caching the next action
+    //     $response = $next($request);
 
-        try {
-            if (! $user = JWTAuth::parseToken()->authenticate() )
-            {
-                return response()->json([
-                   'code'   => 101, // means auth error in the api,
-                   'response' => null // nothing to show 
-                ]);
-            }
-        } catch (TokenExpiredException $e)
-        {
-            // If the token is expired, then it will be refreshed and added to the headers
-            try
-            {
-                $refreshed = JWTAuth::refresh(JWTAuth::getToken());
-                $user = JWTAuth::setToken($refreshed)->toUser();
-                header('Authorization: Bearer ' . $refreshed);
-            }
-            catch (JWTException $e)
-            {
-                 return response()->json([
-                   'code'   => 103, // means not refreshable 
-                   'response' => null // nothing to show 
-                 ]);
-            }
-        }
-        catch (JWTException $e)
-        {
-            return response()->json([
-                   'code'   => 101, // means auth error in the api,
-                   'response' => null // nothing to show 
-            ]);
-        }
-
-        //Auth::login($user, false);
-
-        return $response;
-    }
-
-    // public function handle($request, Closure $next) {
     //     try {
-    //         $user = JWTAuth::parseToken()->authenticate();
     //         if (! $user = JWTAuth::parseToken()->authenticate() )
     //         {
     //             return response()->json([
@@ -69,16 +29,56 @@ class apiProtectedRoute extends BaseMiddleware
     //                'response' => null // nothing to show 
     //             ]);
     //         }
-    //     } 
-    //     catch (\Exception $e) {
-    //         if($e instanceof \Tymon\JWTAuth\Exceptions\TokenInvalidException){
-    //             return response()->json(['status' => 'Token is invalid']);
-    //         } else if($e instanceof \Tymon\JWTAuth\Exceptions\TokenExpiredException) {
-    //             return response()->json(['status' => 'Token is expired']);
-    //         } else {
-    //             return response()->json(['status' => 'Authorization Token not found']);
+    //     } catch (TokenExpiredException $e)
+    //     {
+    //         // If the token is expired, then it will be refreshed and added to the headers
+    //         try
+    //         {
+    //             $refreshed = JWTAuth::refresh(JWTAuth::getToken());
+    //             $user = JWTAuth::setToken($refreshed)->toUser();
+    //             header('Authorization: Bearer ' . $refreshed);
+    //         }
+    //         catch (JWTException $e)
+    //         {
+    //              return response()->json([
+    //                'code'   => 103, // means not refreshable 
+    //                'response' => null // nothing to show 
+    //              ]);
     //         }
     //     }
-    //     return $next($request);
+    //     catch (JWTException $e)
+    //     {
+    //         return response()->json([
+    //                'code'   => 101, // means auth error in the api,
+    //                'response' => null // nothing to show 
+    //         ]);
+    //     }
+
+    //     //Auth::login($user, false);
+
+    //     return $response;
     // }
+
+    public function handle($request, Closure $next) {
+        try {
+            $user = JWTAuth::parseToken()->authenticate();
+            if (! $user = JWTAuth::parseToken()->authenticate() )
+            {
+                return response()->json([
+                   'code'   => 101, // means auth error in the api,
+                   'response' => null // nothing to show 
+                ]);
+            }
+        } 
+        catch (\Exception $e) {
+            if($e instanceof \Tymon\JWTAuth\Exceptions\TokenInvalidException){
+                return response()->json(['status' => 'Token is invalid']);
+            } else if($e instanceof \Tymon\JWTAuth\Exceptions\TokenExpiredException) {
+                return response()->json(['status' => 'Token is expired']);
+            } else {
+                return response()->json(['status' => 'Authorization Token not found']);
+            }
+        }
+        return $next($request);
+    }
 }
